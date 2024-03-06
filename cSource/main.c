be6d9c8181d6ad0c5b38098ddfedbfc0df37519d
程曦 <luohecheng.lhc@alibaba-inc.com>
2024-03-06 20:59:32 +0800

    feat: cmake

--- cSource/main.c
+++ cSource/main.c
@@ -1,5 +1,9 @@
 #include <emscripten.h>
 #include "mycylderfit.h"
+#include "mycylderfit_emxAPI.h"
+#include "mycylderfit_terminate.h"
+#include "mycylderfit_types.h"
+#include "rt_nonfinite.h"
 
 EMSCRIPTEN_KEEPALIVE
 int add(int a, int b)
@@ -7,8 +11,9 @@ int add(int a, int b)
     return a + b + 100;
 }
 
-EMSCRIPTEN_KEEPALIVE
-int exportadd200(int a, int b)
-{
-    return add200(a, b);
-}
+// EMSCRIPTEN_KEEPALIVE
+// int exportadd200(int a, int b)
+// {
+
+//     return add200(a, b);
+// }
