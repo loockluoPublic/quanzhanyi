21ec3ba011432e8a022a7c6f41820e7e85e86861
程曦 <luohecheng.lhc@alibaba-inc.com>
2024-03-08 21:29:25 +0800

    feat: 导出库 构建成功

--- cSource/main.c
+++ cSource/main.c
@@ -1,9 +1,9 @@
 #include <emscripten.h>
-#include "mycylderfit.h"
-#include "mycylderfit_emxAPI.h"
-#include "mycylderfit_terminate.h"
-#include "mycylderfit_types.h"
-#include "rt_nonfinite.h"
+#include "_coder_mycylderfit_api.h"
+// #include "mycylderfit_emxAPI.h"
+// #include "mycylderfit_terminate.h"
+// #include "mycylderfit_types.h"
+// #include "rt_nonfinite.h"
 
 EMSCRIPTEN_KEEPALIVE
 int add(int a, int b)
@@ -11,9 +11,8 @@ int add(int a, int b)
     return a + b + 100;
 }
 
-// EMSCRIPTEN_KEEPALIVE
-// int exportadd200(int a, int b)
-// {
-
-//     return add200(a, b);
-// }
+EMSCRIPTEN_KEEPALIVE
+int add2(int a, int b)
+{
+    return add200(a, b);
+}
