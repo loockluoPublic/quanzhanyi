3abc94269f9e3e59f5624e1b130d952e3fa2b338
程曦 <luohecheng.lhc@alibaba-inc.com>
2024-10-09 23:19:58 +0800

    feat: 去除无用代码

--- src/components/Rectangular.tsx
+++ src/components/Rectangular.tsx
@@ -1,13 +1,6 @@
 import * as THREE from "three";
 import { CustomVector3 } from "../class/CustomVector3";
 import { useMemo } from "react";
-const id = [0, 1, 2];
-
-const ar = [
-  0.5162918275161452, 0.00723298974618225, 0.9857370008742805,
-  0.5326985796083867, 0.00723298974618225, 0.015341215153180876,
-  0.5523981665029891, 0.9943921959776958, 0.015341215153180876,
-];
 
 const Rectangular = (props: { trianglePoints: CustomVector3[] }) => {
   const trianglePoints = props.trianglePoints ?? [];
@@ -26,11 +19,6 @@ const Rectangular = (props: { trianglePoints: CustomVector3[] }) => {
     });
     const vertices = new Float32Array(pointsArr);
 
-    console.log(
-      "%c Line:34 🍧 trianglePoints",
-      "color:#93c0a4",
-      trianglePoints
-    );
     return {
       vertices,
       indices,
